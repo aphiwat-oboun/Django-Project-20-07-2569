b4aaa4ce94516aa33b9b1067510d4fe3b46bf7df
aphiwat-oboun <flim0614759683@gmail.com>
2026-07-20 11:41:20 +0700

    feat: configure Neon PostgreSQL database connection

--- my_web/settings.py
+++ my_web/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,11 +78,13 @@ WSGI_APPLICATION = "my_web.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgresql://neondb_owner:npg_Gr0IdPc8TOJA@ep-purple-base-az3lm9c1-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
 
 
 # Password validation
@@ -116,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "statics"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
